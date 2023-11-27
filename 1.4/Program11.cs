09d684a0119ab89c45f33e6af26be298955cad0f
Ivan Kovach <eeiprex@gmail.com>
2023-11-27 16:28:05 +0300

    [init#1.4] Add new task - 11
    Tasks:
    
        * 11 (Program11.cs)

---
+++ 1.4/Program11.cs
@@ -0,0 +1,45 @@
+using System;
+
+
+class Program
+{
+    public static void Main(string[] argv)
+    {
+        GetN(out Int32 N);  // Вводим N
+        CalcResult(N, out Int32 result);  // Обрабатываем число по условию
+        PrintResult(N, result);  // Выводим результат по условию
+    }
+
+    public static void GetN(out Int32 N)
+    {
+        Console.Write("Введите число N: ");
+        N = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public static void CalcResult(Int32 number, out Int32 result)
+    {
+        result = 0;  // Переменная в которой будет собираться результирующее число
+
+        int sign = (number > 0) ? 1 : -1;  // Сохраняем знак числа, чтобы корректно собрать новое
+        number = Math.Abs(number);  // Далее работаем с положительным значением - так легче
+
+        int count_digits = (int)Math.Floor(Math.Log10(Math.Abs(number))) + 1;  // Берём количество для дальнейших вычислений
+        if (count_digits == 1) {  // Когда число = цифра, то никаких преобраований не нужно
+            result = number;
+            return;
+        }
+
+        int last_digit = number % 10;  // Бёрем последнюю цифру числа (чтобы её умножит на 10 в степени (количество цифр числа)-1 и получить первую
+        int first_digit = number / (int)Math.Pow(10, count_digits - 1);  // Берём первую цифру числа, путём деления на десять в степени индекса последнего разряда числа -1
+
+        Int32 center_part = number / 10 % (int)Math.Pow(10, count_digits - 2);  // Убираем последнюю цифру исходногоо числаи делим на 10 в степени количество цифр-2, т.к. мы стираем 2 цифры
+
+
+        result = sign * ((center_part * 10 + first_digit) + (last_digit * (int)Math.Pow(10, count_digits - 1)));  // Результат преобразований первого числа
+    }
+
+    public static void PrintResult(Int32 number, Int32 result)
+    {
+        Console.WriteLine($"{number} => {result}");
+    }
+}
