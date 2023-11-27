5280d80e70d6c35822f89a8cd538ff5df8568602
Ivan Kovach <eeiprex@gmail.com>
2023-11-27 16:28:42 +0300

    [init#1.4] Add new task - 14
    Tasks:
    
        * 14 (Program14.cs)

---
+++ 1.4/Program14.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+class Program
+{
+    public static void Main(string[] argv)
+    {
+        GetNumber(out Int32 N);
+        CalcResult(N, out Int32 result);
+        PrintResult(N, result);
+    }
+
+    public static void GetNumber(out Int32 n)
+    {
+        Console.Write("Введите натуральное число N: ");
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public static void CalcResult(Int32 n, out Int32 result)
+    {
+        result = 0;
+
+        for (int i = 0; i <= n; ++i) {
+            result += NumberIsOdd(i) ? i : -i;
+        }
+    }
+
+    public static void PrintResult(Int32 n, Int32 result)
+    {
+        Console.WriteLine($"Исходное значение N: {n}");
+        Console.WriteLine($"Сумма ряда знакачередующегося ряда: {result}");
+    }
+
+    private static bool NumberIsOdd(Int32 number)
+    {
+        return number % 2 != 0;
+    }
+}
