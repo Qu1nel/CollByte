de3e7d71ba9f3812f79bb6106adda8fc02077236
Ivan Kovach <eeiprex@gmail.com>
2023-11-27 20:31:03 +0300

    [chore#1.4] Update task 14 - add validation input value

--- 1.4/Program14.cs
+++ 1.4/Program14.cs
@@ -12,16 +12,21 @@ class Program
 
     public static void GetNumber(out Int32 n)
     {
-        Console.Write("Введите натуральное число N: ");
-        n = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите натуральное число N: ");  // Выводим приглашение к вводу
+        string input = Console.ReadLine() ?? "0";  // Если ничего не ввели - значение по умолчанию 0
+
+        if (!Int32.TryParse(input, out n)) {  // Пытаемся привести строку к числу
+            Console.WriteLine("Не корректный тип! Дотустимый тип ввода для числа - целочисленный.");  // Предупреждение в случае, если строка не число
+            Environment.Exit(1);  // Выход с кодом возврата 1
+        }
     }
 
     public static void CalcResult(Int32 n, out Int32 result)
     {
-        result = 0;
+        result = 0;  // ложим начальное значение суммы ряда
 
         for (int i = 0; i <= n; ++i) {
-            result += NumberIsOdd(i) ? i : -i;
+            result += NumberIsOdd(i) ? i : -i;  // Каждое нечетное число - положительное, а четное - отрицательное
         }
     }
 
@@ -33,6 +38,6 @@ class Program
 
     private static bool NumberIsOdd(Int32 number)
     {
-        return number % 2 != 0;
+        return number % 2 != 0;  // У чётных чисел отстаток от деления на 2 должен равняться 0, у нечётных - 1
     }
 }
