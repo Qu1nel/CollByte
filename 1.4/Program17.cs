a2ffddaabe0299b387e6715da1746757bf39cb14
Ivan Kovach <eeiprex@gmail.com>
2023-11-27 20:35:33 +0300

    [chore#1.4] Update task 17 - add validation input value

--- 1.4/Program17.cs
+++ 1.4/Program17.cs
@@ -5,28 +5,33 @@ class Program
 {
     public static void Main(string[] argv)
     {
-        GetNumber(out Int32 N);
-        CalcResult(N, out bool NumberInSerial);
-        PrintResult(N, NumberInSerial);
+        GetNumber(out Int32 N);  // Ввод натурального числа N 
+        CalcResult(N, out bool NumberInSerial);  // Вычисление чисел фиббоначи и проверка по условию задачи
+        PrintResult(N, NumberInSerial);  // Вывод результата
     }
 
     public static void GetNumber(out Int32 n)
     {
-        Console.Write("Введите натуральное число N: ");
-        n = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите натуральное число N: ");  // Выводим приглашение к вводу
+        string input = Console.ReadLine() ?? "0";  // Если ничего не ввели - значение по умолчанию 0
+
+        if (!Int32.TryParse(input, out n)) {  // Пытаемся привести строку к числу
+            Console.WriteLine("Не корректный тип! Дотустимый тип ввода для числа - целочисленный.");  // Предупреждение в случае, если строка не число
+            Environment.Exit(1);  // Выход с кодом возврата 1
+        }
     }
 
-    public static void CalcResult(Int32 n, out bool In)
+    public static void CalcResult(Int32 n, out bool In)  // Метод вычисления чисел фибоначчи пока вычисляемое число не привысит данное
     {
-        Int64 f1 = 0, f2 = 1, f = 1;
+        Int64 f1 = 0, f2 = 1, f = 1;  // Заводим переменные для вычисления чисел f1 и f2
 
-        while (f1 < n) {
+        while (f1 < n) {  // Пока вычисляемое число не привысит данное - вычисляем следующее
             f1 = f2;
             f2 = f;
             f = f1 + f2;
         }
 
-        In = n == f1;
+        In = n == f1;  // Кладём в результирующую переменную результат: является ли число элементом ряда фибоначчи
     }
 
     public static void PrintResult(Int32 n, bool belongs)
